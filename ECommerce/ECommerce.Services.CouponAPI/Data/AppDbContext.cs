adb4a32ce600cedf03afeb7599d75308d123a6fb
2100867A <williamsu@softmobile.com.tw>
2024-06-19 16:02:36 +0800

    [ADD] 16. Create AppDbContext

---
+++ ECommerce/ECommerce.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Services.CouponAPI.Data
+{
+    public class AppDbContext:DbContext
+    {   
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
