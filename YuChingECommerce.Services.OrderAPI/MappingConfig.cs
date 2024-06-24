f73bc5c9d8f5b293c708420028c000459dd55c41
2100867A <williamsu@softmobile.com.tw>
2024-06-24 14:44:33 +0800

    [ADD] 訂單 API 開發完成

---
+++ YuChingECommerce.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using YuChingECommerce.Services.OrderAPI.Models;
+using YuChingECommerce.Services.OrderAPI.Models.Dto;
+
+namespace YuChingECommerce.Services.OrderAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<OrderHeaderDto, CartHeaderDto>()
+                .ForMember(dest=>dest.CartTotal, u=>u.MapFrom(src=>src.OrderTotal)).ReverseMap();
+
+                config.CreateMap<CartDetailsDto, OrderDetailsDto>()
+                .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+
+                config.CreateMap<OrderDetailsDto, CartDetailsDto>();
+
+                config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+                config.CreateMap<OrderDetailsDto, OrderDetails>().ReverseMap();
+
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ YuChingECommerce.Services.OrderAPI/Service/IService/IProductService.cs
@@ -0,0 +1,10 @@
+﻿
+using YuChingECommerce.Services.OrderAPI.Models.Dto;
+
+namespace YuChingECommerce.Services.ShoppingCartAPI.Service.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+    }
+}
