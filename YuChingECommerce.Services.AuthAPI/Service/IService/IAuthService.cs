dab8f047f379c9a97e74d16a23cfe815d85a54e4
2100867A <williamsu@softmobile.com.tw>
2024-06-24 14:43:32 +0800

    [ADD] 身分驗證 API 開發完成

---
+++ YuChingECommerce.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using YuChingECommerce.Services.AuthAPI.Models.Dto;
+
+namespace YuChingECommerce.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegistrationRequestDto registrationRequestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<bool> AssignRole(string email, string roleName);
+    }
+}
---
+++ YuChingECommerce.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using YuChingECommerce.Services.AuthAPI.Models;
+
+namespace YuChingECommerce.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles);
+    }
+}
