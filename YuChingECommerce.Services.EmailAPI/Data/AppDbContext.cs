afc4b0ddc6bebe6bb5eff7f0430283f47c382cd6
2100867A <williamsu@softmobile.com.tw>
2024-07-09 15:53:50 +0800

    [ADD] Email API 開發完成

---
+++ YuChingECommerce.Services.EmailAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using YuChingECommerce.Services.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace YuChingECommerce.Services.EmailAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+
+        
+    }
+}
---
+++ YuChingECommerce.Services.EmailAPI/Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using YuChingECommerce.Services.EmailAPI.Message;
+using YuChingECommerce.Services.EmailAPI.Models.Dto;
+
+namespace YuChingECommerce.Services.EmailAPI.Services
+{
+    public interface IEmailService
+    {
+        Task EmailCartAndLog(CartDto cartDto);
+        Task RegisterUserEmailAndLog(string email);
+        Task LogOrderPlaced(RewardsMessage rewardsDto);
+    }
+}
