9694848875f7009363ac3592553c3e75b9b5998c
2100867A <williamsu@softmobile.com.tw>
2024-07-09 15:54:25 +0800

    [ADD] 產品 API 開發完成

---
+++ YuChingECommerce.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using YuChingECommerce.Services.ProductAPI.Models;
+using YuChingECommerce.Services.ProductAPI.Models.Dto;
+
+namespace YuChingECommerce.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
